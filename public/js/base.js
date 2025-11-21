5fa6ec3945aa22726f19559147e879faf8fcfe50
Giovanna Izac Silva <giovannaizac2@hotmail.com>
2025-11-21 18:07:23 -0300

    Iniciando o Git

---
+++ public/js/base.js
@@ -0,0 +1,29 @@
+const nome = "Giovanna Izac";
+let nome2 = "";
+let pessoaDefault 
+
+function alterarNome() {
+    nome2: "Maria Silva"
+    console.log("Valor alterado");
+    console.log(nome2);
+}
+
+function recebeEalteraNome(novoNome){
+    nome2 = novoNome;
+    console.log("Valor alterado recebendo um nome")
+    console.log(nome2);
+}
+
+function imprimirPessoa(pessoa) {
+    console.log("Nome");
+    console.log("pessoa.nome");
+
+    console.log("Idade");
+    console.log("pessoa.idade");
+
+    console.log("Trabalho");
+    console.log("pessoa.trabalho");
+}
+
+imprimirPessoa(pessoaDefault);
+
